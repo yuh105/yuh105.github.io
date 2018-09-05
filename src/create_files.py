977c42fa54fcba4f06e526c7c2fb44357265de89
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-09-05 13:18:13 +0000

    remove brbrbr

--- src/create_files.py
+++ src/create_files.py
@@ -1,29 +1,4 @@
-def create_from_html(write_file, read_file_path):
-    with open(read_file_path, mode='r') as rfile:
-        wfile.write('function ' + read_file_path.split('/')[-1].split('.')[0] + '() {')
-        wfile.write("let html = '';")
-        pre_flag = False
-        while True:
-            line = rfile.readline()
-            if not line:
-                break
-
-            if '<pre>' in line:
-                pre_flag = True
-                print(line)
-            if '</pre>' in line:
-                pre_flag = False
-                print(line)
-
-            if pre_flag:
-                wfile.write("html += '" + line.strip() + "\\n';")
-            else:
-                wfile.write("html += '" + line.strip() + "';")
-        wfile.write("document.write(html);")
-        wfile.write('}')
-
-
-def create_from_jcss(write_file, read_file_path):
+def create(write_file, read_file_path):
     with open(read_file_path, mode='r') as rfile:
         while True:
             line = rfile.readline()
@@ -33,6 +8,8 @@ def create_from_jcss(write_file, read_file_path):
 
 
 if __name__ == '__main__':
+    write_html = './../index.html'
+    index_html = 'files/index.html'
 
     write_css = './../css/style.css'
     css = 'files/style.css'
@@ -43,12 +20,15 @@ if __name__ == '__main__':
     nav_butons_js = 'files/nav_buttons.js'
     create_square_in_square_js = 'files/createSquareInSquare.js'
 
+    with open(write_html, mode='w') as wfile:
+        create(wfile, index_html)
+
     with open(write_css, mode='w') as wfile:
-        create_from_jcss(wfile, css)
+        create(wfile, css)
 
     with open(write_js, mode='w') as wfile:
-        create_from_jcss(wfile, models_js)
-        create_from_jcss(wfile, myscript_js)
-        create_from_jcss(wfile, nav_butons_js)
-        create_from_jcss(wfile, create_square_in_square_js)
+        create(wfile, models_js)
+        create(wfile, myscript_js)
+        create(wfile, nav_butons_js)
+        create(wfile, create_square_in_square_js)
 
